46076d8fd0b25502a3ce1b76fbee8028907fba46
maria-nusrat-2005 <marianusrat.tonu34@gmail.com>
2025-09-06 01:09:57 +0600

    call-button section done

--- A5/script.js
+++ A5/script.js
@@ -4,3 +4,20 @@ function heartCounting(id) {
   heartCount += 1;
   document.getElementById("heart-count").innerText = heartCount;
 }
+
+function handleCallButton(button) {
+  const card = button.closest(".card");
+  const serviceName = card.querySelector(".service-name").innerText;
+  const serviceNumber = card.querySelector(".service-num").innerText;
+  let coins = parseInt(document.getElementById("coin-count").innerText);
+  if (coins < 20) {
+    alert("Not Enough Point");
+    return;
+  }
+  coins -= 20;
+  document.getElementById("coin-count").innerText = coins;
+  console.log(serviceName, serviceNumber);
+  alert(`${serviceName}, ${serviceNumber}`);
+}
+
+function updateLog(serviceName, serviceNumber) {}
