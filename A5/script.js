d5ab72d1ff71c37ad225cbe22283cee89020addc
maria-nusrat-2005 <marianusrat.tonu34@gmail.com>
2025-09-06 01:36:13 +0600

    clear call history done

--- A5/script.js
+++ A5/script.js
@@ -18,6 +18,28 @@ function handleCallButton(button) {
   document.getElementById("coin-count").innerText = coins;
   console.log(serviceName, serviceNumber);
   alert(`${serviceName}, ${serviceNumber}`);
+  updateLog(serviceName, serviceNumber);
 }
 
-function updateLog(serviceName, serviceNumber) {}
+function updateLog(serviceName, serviceNumber) {
+  const logContainer = document.getElementById("logs-container");
+  const logDiv = document.createElement("div");
+
+  logDiv.innerHTML = `   <div id="log" class="rounded-lg
+   bg-gray-100 flex p-3 justify-between items-center mt-3">
+              <div class="">
+                   <h4 class="font-bold">${serviceName}</h4>
+                   <p>${serviceNumber}</p>
+              </div>
+              <p>11:36:58 AM</p>
+
+            </div>`;
+  console.log(logDiv);
+  console.log(logContainer);
+
+  logContainer.appendChild(logDiv);
+}
+
+function clearHistory() {
+  document.getElementById("logs-container").innerHTML = "";
+}
